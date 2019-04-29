020be4e1bfce030d268a4a394b5e0d32d0c80510
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-04-29 23:21:31 +0200

    feat(number): find prime implementation

--- number/prime.test.ts
+++ number/prime.test.ts
@@ -1,10 +1,20 @@
 import * as Number from './';
 
-test('Test if an integer is a prime number', () => {
+test('Check if integer between 2 and 2^16 is a prime number', () => {
+  expect(Number.isPrime(2)).toBe(true);
+  expect(Number.isPrime(9)).toBe(false);
   expect(Number.isPrime(19)).toBe(true);
   expect(Number.isPrime(110)).toBe(false);
   expect(Number.isPrime(367)).toBe(true);
   expect(Number.isPrime(2381)).toBe(true);
   expect(Number.isPrime(1290)).toBe(false);
   expect(Number.isPrime(35090)).toBe(false);
+});
+
+
+test('Find the th prime number', () => {
+  expect(Number.findPrime(9)).toBe(23);
+  expect(Number.findPrime(100)).toBe(541);
+  expect(Number.findPrime(1000)).toBe(7919);
+  // expect(Number.findPrime(10000)).toBe(7919);
 });
--- number/prime.ts
+++ number/prime.ts
@@ -9,7 +9,32 @@ export const isPrime = (n: number): boolean => {
   if (n < 2 || n > 2 ** 16) {
     return false;
   }
-  for (let counter = 2; counter < n; counter +=1 ) {
-    return n % counter === 0 ? false : true;
+
+  if (n === 2) {
+    return true
   }
+  for (let counter = 2; counter < n; counter++ ) {
+    if (n % counter === 0) {
+      return false;
+    }
+  }
+  return true
 };
+
+/**
+ * @name findPrime
+ * @description Have the findPrime(num) return the numth prime number.
+ * The range will be from 1 to 10^4.
+ * For example: if num is 16 the output should be
+ * 53 as 53 is the 16th prime number.
+ */
+export const findPrime = (n: number): number => {
+
+  const primes = [];
+  for(let counter = 2; primes.length < n; counter++) {
+    if (isPrime(counter)) {
+      primes.push(counter);
+    }
+  }
+  return primes[n - 1];
+}
