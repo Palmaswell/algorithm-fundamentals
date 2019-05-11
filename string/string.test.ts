3edc0ef9ceea56c6e981c55376755fb6890c51de
Mauricio Palma <mauricio.palma@sinnerschrader.com>
2019-05-11 20:46:10 +0200

    feat(string): implement count minutes function

--- string/string.test.ts
+++ string/string.test.ts
@@ -1,18 +1,20 @@
 import * as String from './';
 
 test('returns the greatest common divisor between two Strings', () => {
-  expect(String.inScramble('coodrebtqqkye','coderbyte')).toBe(true);
+  expect(String.inScramble('coodrebtqqkye', 'coderbyte')).toBe(true);
   expect(String.inScramble('thsisifasl3', 'thisisfalse')).toBe(false);
   expect(String.inScramble('lettrrkakaeaaaqq', 'letter')).toBe(true);
   expect(String.inScramble('aqwe', 'qa')).toBe(true);
   expect(String.inScramble('yelowrqwedlk', 'yellowred')).toBe(true);
   expect(String.inScramble('abcgggdfe', 'abcdef')).toBe(true);
   expect(String.inScramble('win33er', 'winner')).toBe(false);
-})
+});
 
 test('Find the word with the most characters repetitions', () => {
   expect(String.letterCount('Hello apple pie')).toBe('Hello');
-  expect(String.letterCount('Today, is the greatest day ever!')).toBe('greatest');
+  expect(String.letterCount('Today, is the greatest day ever!')).toBe(
+    'greatest'
+  );
   expect(String.letterCount('No words')).toBe(-1);
   expect(String.letterCount('None yellow')).toBe('None');
 });
@@ -23,4 +25,12 @@ test('Caesar cipher shift the words of a string according to the given number pa
   expect(String.ceasarsCipher('what', 2)).toBe('yjcv');
   expect(String.ceasarsCipher('dogs', 8)).toBe('lwoa');
   expect(String.ceasarsCipher('byte', 13)).toBe('olgr');
+});
+
+test('Returns the difference in minutes of two time periods in 12hr format', () => {
+  expect(String.countMinutes('12:30pm-12:00am')).toBe(690);
+  expect(String.countMinutes('1:23am-1:08am')).toBe(1425);
+  expect(String.countMinutes('11:00am-2:10pm')).toBe(190);
+  expect(String.countMinutes('12:31pm-12:34pm')).toBe(3);
+  expect(String.countMinutes('5:00pm-5:11pm')).toBe(11);
 });
--- string/string.ts
+++ string/string.ts
@@ -12,14 +12,14 @@
 
 export const inScramble = (str1: string, str2: string): boolean => {
   let stringBase = str1.split('');
-  for(let i = 0; i < str2.length; i++) {
+  for (let i = 0; i < str2.length; i++) {
     if (stringBase.indexOf(str2[i]) === -1) {
       return false;
     }
     stringBase.splice(stringBase.indexOf(str2[i]), 1);
   }
   return true;
-}
+};
 
 /**
  * @name letterCount
@@ -35,37 +35,98 @@ export interface ResultProps {
   score: number;
 }
 
- export const letterCount = (str: string): LetterCountType => {
-   const sentence = str.split(' ');
-   const result = sentence.map((word: string): number => (
-     word.split('')
-      .map((char: string): number => {
-        const test = new RegExp(char, 'gi');
-        const score = word.match(test).length;
-        if (score === 1) {
-          return 0;
-        }
-        return score;
-      }).reduce((acc, curr) => acc + curr)
-   ))
-   .map((score: number, index: number) => ({score, index}))
-   .sort((el1, el2) => el2.score - el1.score);
+export const letterCount = (str: string): LetterCountType => {
+  const sentence = str.split(' ');
+  const result = sentence
+    .map(
+      (word: string): number =>
+        word
+          .split('')
+          .map(
+            (char: string): number => {
+              const test = new RegExp(char, 'gi');
+              const score = word.match(test).length;
+              if (score === 1) {
+                return 0;
+              }
+              return score;
+            }
+          )
+          .reduce((acc, curr) => acc + curr)
+    )
+    .map((score: number, index: number) => ({ score, index }))
+    .sort((el1, el2) => el2.score - el1.score);
 
-   if (result[0].score === 0) {
-     return -1;
-   }
-   return sentence[result[0].index];
- }
+  if (result[0].score === 0) {
+    return -1;
+  }
+  return sentence[result[0].index];
+};
 
- /**
-  * @name ceasarsCipher
-  * @description
-  */
+/**
+ * @name ceasarsCipher
+ * @description
+ */
 
 export const ceasarsCipher = (str: string, n: number): string => {
-  const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  return str.split('')
+  const lowerCase = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  const upperCase = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  return str
+    .split('')
     .map((char: string) => {
       if (!/[A-Za-z]/g.test(char)) {
         return char;
@@ -75,7 +136,56 @@ export const ceasarsCipher = (str: string, n: number): string => {
         shiftPosition -= lowerCase.length;
       }
       return /[A-Z]/g.test(char)
-       ? upperCase[shiftPosition]
-       : lowerCase[shiftPosition];
-    }).join('');
+        ? upperCase[shiftPosition]
+        : lowerCase[shiftPosition];
+    })
+    .join('');
+};
+
+/**
+ * @name countMinutes
+ * @description Have the function countMinutes(str) take the str parameter
+ * being passed which will be two times (each properly formatted with a colon
+ * and am or pm) separated by a hyphen and return the total number of minutes
+ * between the two times.
+ * The time will be in a 12 hour clock format. For example: if str is
+ * 9:00am-10:00am then the output should be 60. If str is 1:00pm-11:00am
+ * the output should be 1320.
+ */
+enum Period {
+  am = 'am',
+  pm = 'pm',
 }
+interface Time {
+  hours: number;
+  period: Period;
+}
+
+const formatTime = (time: string): string =>
+  time
+    .replace(/[ampm]/g, '')
+    .split(':')
+    .reduce(
+      (acc: string, curr: string): string => {
+        const minutes = (parseInt(curr, 10) / 60).toString().slice(2);
+        return `${acc}.${minutes}`;
+      }
+    );
+
+const createTime = (time: string): Time => ({
+  hours: parseFloat(formatTime(time)),
+  period: /am/g.test(time) ? Period.am : Period.pm,
+});
+
+export const countMinutes = (str: string): number => {
+  const timeArr = str.split('-');
+  const time1 = createTime(timeArr[0]);
+  const time2 = createTime(timeArr[1]);
+  const diff = time2.hours - time1.hours;
+  if (time1.period === time2.period) {
+    return time2.hours > time1.hours
+      ? Math.round(diff * 60)
+      : Math.round((24 + diff) * 60);
+  }
+  return Math.round((diff + 12) * 60);
+};
